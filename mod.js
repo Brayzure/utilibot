1eec1f8991fb5c2d74bf09cc4767049f0ee6e7ee
Brayzure <rictylp@gmail.com>
2017-02-22 22:37:25 -0500

    Utility Functions - Added permission calculator - Added function for determining if the bot can mod the current guild - Added moderation mutex for future use

--- mod.js
+++ mod.js
@@ -14,6 +14,12 @@ const auth = require('./src/auth.json');
 const config = require('./src/config.json');
 
 /*
+* SESSION VARIABLES
+*/
+// Users currently in the process of being moderated
+var modMutex = {};
+
+/*
 * CLIENT CREATION AND CONNECTION
 */
 var client = new Eris(auth.token, {
@@ -64,15 +70,22 @@ client.on('ready', () => {
 });
 
 client.on('messageCreate', (m) => {
+	// Client not ready, let's avoid strange behavior
 	if(!client.ready) return;
+
+	// PM not from the bot itself
 	if(!m.channel.guild && m.author.id !== client.user.id) {
 		let str = `New PM\nAuthor: ${m.author.username} (${m.author.id})\nContent: ${m.content}`;
 		log('pm', str);
 	}
+
+	// Not a PM
 	else {
 		
 	}
 });
+
+
 
 function log(location, content) {
 	if(location === 'botlog') {
@@ -116,4 +129,62 @@ function timestamp(time) {
 		d = new Date();
 	}
 	let str = `${d.getDate()}/${d.getMonth()+1}/${d.getFullYear()}`
+}
+
+function canIMod(guild) {
+	let perms = permissions(guild, guild.channels.get(guild.id), client.user);
+	let neededPerms = ['kickMembers', 'banMembers', 'manageMessages'];
+	let mod = true;
+	for(let i=0; i<neededPerms.length && mod; i++) {
+		if(!~perms.indexOf(neededPerms[i])) mod = false;
+	}
+	return mod;
+}
+
+function permissions(guild, channel, user) {
+	let obj = {};
+	let roles = guild.members.get(user.id).roles;
+	if(!roles) return []; // Failed to find member info, assume no permissions
+
+	// Determine channel overrides
+	channel.permissionOverwrites.forEach((over) => {
+		if(roles.includes(over.id) || over.id === guild.id) {
+			let j = over.json;
+			for(let key in j) {
+				if(j.hasOwnProperty(key) && (!obj.hasOwnProperty(key) || j[key])) {
+					obj[key] = j[key];
+				}
+			}
+		}
+	});
+	
+	// Determine global permissions
+	let r = guild.roles.get(guild.id); // Default permissions
+	let j = r.permissions.json;
+	for(let key in j) {
+		if(j.hasOwnProperty(key) && !obj.hasOwnProperty(key) && j[key]) {
+			obj[key] = true;
+		}
+	}
+	for(let i in roles) { // Permissions from roles
+		r = guild.roles.get(roles[i]);
+		j = r.permissions.json;
+		for(let key in j) {
+			if(j.hasOwnProperty(key) && j[key]) {
+				obj[key] = true;
+			}
+		}
+	}
+
+	// Convert object to array
+	let arr = [];
+	for(let key in obj) {
+		if(obj.hasOwnProperty(key) && obj[key]) {
+			arr.push(key);
+		}
+	}
+	if(user.id === auth.dev_id) {
+		arr.push('manageServer', 'developer');
+	}
+	return arr;
 }
