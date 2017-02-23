17af4c279851ad0bdd269c8ef5cb93f75b020838
Brayzure <rictylp@gmail.com>
2017-02-22 22:11:25 -0500

    Add Sharding and Logging - Sharding value to be set in config.json - Logging \o/

--- mod.js
+++ mod.js
@@ -14,5 +14,106 @@ const auth = require('./src/auth.json');
 const config = require('./src/config.json');
 
 /*
-* TODO: Everything
+* CLIENT CREATION AND CONNECTION
 */
+var client = new Eris(auth.token, {
+	getAllUsers: true,
+	disableEvents: {
+		PRESENCE_UPDATE: true
+	},
+	maxShards: config.shards
+});
+
+var pg = new Postgres.Client({
+	user: auth.pg_user,
+	password: auth.pg_password,
+	database: auth.pg_db
+});
+
+client.connect();
+
+/*
+* EVENTS
+*/
+client.on('shardReady', (id) => {
+	if(client.ready) {
+		log('botlog', `Shard ${id} ready!`);
+	}
+	else {
+		log('console', `Shard ${id} ready!`);
+	}
+});
+
+client.on('shardDisconnect', (err, id) => {
+	let str;
+	if(err) {
+		str = `Shard ${id} disconnected with this error: ${err}`;
+	}
+	else {
+		str = `Shard ${id} disconnected without errors.`;
+	}
+	log('botlog', str);
+});
+
+client.on('shardResume', (id) => {
+	log('botlog', `Shard ${id} has successfully resumed!`);
+});
+
+client.on('ready', () => {
+	log('botlog', `Client has finished launching ${client.shards.size} shards!`);
+});
+
+client.on('messageCreate', (m) => {
+	if(!client.ready) return;
+	if(!m.channel.guild && m.author.id !== client.user.id) {
+		let str = `New PM\nAuthor: ${m.author.username} (${m.author.id})\nContent: ${m.content}`;
+		log('pm', str);
+	}
+	else {
+		
+	}
+});
+
+function log(location, content) {
+	if(location === 'botlog') {
+		if(client.ready) {
+			client.createMessage(config.error_channel, {embed: {
+				description: content
+			}}).catch(console.log);
+		}
+		console.log(content);
+	}
+	if(location === 'info') {
+		if(client.ready) {
+			client.createMessage(config.log_channel, {embed: {
+				description: content
+			}}).catch(console.log);
+		}
+		console.log(content);
+	}
+	if(location === 'pm') {
+		if(client.ready) {
+			client.createMessage(config.pm_channel, {embed: {
+				description: content
+			}}).catch(console.log);
+		}
+		console.log(content);
+	}
+	if(location === 'console') {
+		console.log(content);
+	}
+	if(location === 'debug' && config.debug) {
+		console.log(`Debug: ${content}`);
+	}
+}
+
+function timestamp(time) {
+	let d;
+	if(time) {
+		d = new Date(time);
+	}
+	else {
+		d = new Date();
+	}
+	let str = `${d.getDate()}/${d.getMonth()+1}/${d.getFullYear()}`
+}
